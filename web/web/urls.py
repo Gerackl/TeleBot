bd7cc7699348c59205ab4f3617e8cee180cc9ca3
Gerackl <81320501+Gerackl@users.noreply.github.com>
2022-04-22 15:52:23 +0300

    Update urls.py
    Home.html теперь работает

--- web/web/urls.py
+++ web/web/urls.py
@@ -21,6 +21,7 @@ urlpatterns = []
 
 urlpatterns += [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("sign_up/", include("Users.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
