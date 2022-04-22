1d5cc71af75854d6efd875ac6881f5a4cfb1bbfc
Gerackl <81320501+Gerackl@users.noreply.github.com>
2022-04-22 12:53:41 +0300

    Починил работу home.html
    добавил недостающий url, не уверен, но возможно надо убрать его в Users/urls.py

--- web/web/urls.py
+++ web/web/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from Projects import views
+from Users.views import SignUpView
 urlpatterns = []
 
 urlpatterns += [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("sign_up/", SignUpView.as_view(), name = "signup"),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
